f8130053a6c5ddfed431c26c98df352f17803f32
Alexandr Anashkin <alexanash92@gmail.com>
2016-08-07 21:22:17 +0300

    'add_if_error'

--- server/index.js
+++ server/index.js
@@ -22,6 +22,9 @@ app.listen(app.get('port'), function() {
 
 app.get('/db', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        if (err) {
+            console.log('err = ', err);
+        }
         client.query('SELECT * FROM test_table', function(err, result) {
             done();
             if (err)
