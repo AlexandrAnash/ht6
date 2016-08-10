647b8543cc0b3e72412ae414c8ccc7229d6bd13b
Alexandr Anashkin <alexanash92@gmail.com>
2016-08-11 02:02:55 +0300

    tests, logic, refactor

---
+++ server/app/index.js
@@ -0,0 +1,35 @@
+'use strict';
+import {TaskModel} from './TaskModel';
+/**
+ * @file
+ * Сервер приложения. Основан на примере для Heroku
+ */
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const pg = require('pg');
+
+const listData = [
+    new TaskModel(0,1,2),
+    new TaskModel(2,1,0)
+];
+app.set('port', process.env.PORT || 5000);
+app.use(express.static(path.join(__dirname, '../../client-src/app')));
+app.use(bodyParser.json());
+
+app.get('/', function(request) {
+    console.time('render', request);
+    console.timeEnd('render');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Cool faces on port', app.get('port'));
+});
+
+app.get('/getTestData', function (request, response) {
+    response.json(listData);    
+});
+app.post('/log', function(request, response) {
+    console.error('user error ',request.body);
+});
---
+++ server/test/taskModelTest.js
@@ -0,0 +1,19 @@
+import {TaskModel} from '../app/TaskModel';
+import {TaskFactory} from '../app/TaskFactory';
+
+describe('Test task model', function() {
+    it('should be right a constructor', function() {
+        TaskFactory.isCorrect = function(){return true;};
+        let taskModel = new TaskModel(1,2,3);
+        expect(taskModel.red).toBe(1);
+        expect(taskModel.green).toBe(2);
+        expect(taskModel.blue).toBe(3);
+    });
+    
+    it('should be throw if bad params', function() {
+        TaskFactory.isCorrect = function(){return false;};
+        expect(function() {
+            new TaskModel(1,2,3);
+        }).toThrowError('no valid input data: red = 1, green = 2, blue = 3');
+    });
+});
